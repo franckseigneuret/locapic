ffd3bcb92737dcea604b2b62d78c37843e754fc8
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-02-16 21:43:20 +0100

    empecher masquer champs saisi par keyboard

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, KeyboardAvoidingView } from 'react-native';
 import { ListItem, Button, Input } from 'react-native-elements';
 import { Ionicons, EvilIcons } from '@expo/vector-icons';
 
@@ -18,38 +18,42 @@ const list = [
 const ChatScreen = (props) => {
   return (
     <View style={{ flex: 1, justifyContent: 'space-between', backgroundColor: '#f2f2f2' }}>
-      <View style={{ width: '100%', marginTop: 55}}>
-      {
-        list.map((l, i) => (
-          <ListItem key={i} bottomDivider style={{ width: '100%' }}>
-            <ListItem.Content>
-              <ListItem.Title>{l.name}</ListItem.Title>
-              <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
-            </ListItem.Content>
-          </ListItem>
-        ))
-      }
+      <View style={{ width: '100%', marginTop: 55 }}>
+        {
+          list.map((l, i) => (
+            <ListItem key={i} bottomDivider style={{ width: '100%' }}>
+              <ListItem.Content>
+                <ListItem.Title>{l.name}</ListItem.Title>
+                <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          ))
+        }
       </View>
 
-      <View style={{ width: '100%'}}>
-        <Input
-          placeholder='Your message'
-          containerStyle={styles.name}
-        />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
 
-        <Button
-          title="Send"
-          icon={
-            <EvilIcons name="envelope" size={24} color="#fff" />
-          }
-          iconLeft
-          type="solid"
-          buttonStyle={{
-            backgroundColor: '#da5951'
-          }}
-          onPress={() => props.navigation.navigate('Main', { screen: 'Map' })}
-        />
-      </View>
+          <Input
+            placeholder='Your message'
+            containerStyle={styles.name}
+          />
+
+          <Button
+            title="Send"
+            icon={
+              <EvilIcons name="envelope" size={24} color="#fff" />
+            }
+            iconLeft
+            type="solid"
+            buttonStyle={{
+              backgroundColor: '#da5951'
+            }}
+            onPress={() => props.navigation.navigate('Main', { screen: 'Map' })}
+          />
+
+      </KeyboardAvoidingView>
     </View>
   )
 }
