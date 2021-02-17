23d67d8ea3aa60c76bcfb31d5a40aa1b60f06b5c
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-02-17 11:26:25 +0100

    suivi localisation du marqueur

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 const MapScreen = (props) => {
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [currentLatitude, setCurrentLatitude] = useState(0)
+  const [currentLongitude, setCurrentLongitude] = useState(0)
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('L\'autorisation d\'accéder à l\'emplacement a été refusée');
+        return;
+      }
+
+      Location.watchPositionAsync({ distanceInterval: 2 },
+        (location) => {
+          // console.log(location);
+  
+          setCurrentLatitude(location.coords.latitude)
+          setCurrentLongitude(location.coords.longitude)
+        }
+      );
+
+    })();
+  }, []);
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#f2f2f2' }}>
       <MapView style={{ flex: 1, width: '100%' }}
@@ -13,7 +38,8 @@ const MapScreen = (props) => {
           longitudeDelta: 0.0421,
         }}
       >
-        <Marker coordinate={{ latitude: 48.858370, longitude: 2.294481 }} />
+        <Marker coordinate={{ latitude: currentLatitude, longitude: currentLongitude }} title="Hello"
+          description="I am here !" />
       </MapView>
     </View>
   )
