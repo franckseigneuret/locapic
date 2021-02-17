45cbcd4682e93e941d1f3119c668738ab80a3a7c
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-02-17 11:07:15 +0100

    ajout carte sur mapscreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 const MapScreen = (props) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'#f2f2f2'}}>
-     <Text>MapScreen</Text>
-   </View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#f2f2f2' }}>
+      <MapView style={{ flex: 1, width: '100%' }}
+        initialRegion={{
+          latitude: 48.866667,
+          longitude: 2.333333,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker coordinate={{ latitude: 48.858370, longitude: 2.294481 }} />
+      </MapView>
+    </View>
   )
 }
 export default MapScreen;
